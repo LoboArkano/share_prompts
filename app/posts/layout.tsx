8e6b0b57cd9dec875038226b85845a69d65dc536
LoboArkano <pipiolo_mananes@hotmail.com>
2023-06-14 14:43:13 -0600

    Restart app folder

--- app/posts/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const layout = () => {
-  return (
-    <div>Navigate to Top</div>
-  )
-}
--- app/posts/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const loading = () => {
-  return (
-    <>
-      <h1>Loading</h1>
-    </>
-  )
-}
